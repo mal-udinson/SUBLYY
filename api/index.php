a9d1009102b65264468c61a8d8471b4d3ce7f22e
akmal <akmaludin2333@gmail.com>
2026-06-29 16:16:35 +0700

    fix: clear local artisan cache and remove config cache files

--- api/index.php
+++ api/index.php
@@ -1,20 +1,15 @@
 <?php
 
-// 1. Aktifkan penanganan error mentah
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// 2. Muat otomatis komponen (Autoload) bawaan Laravel
+// 1. Muat otomatis komponen (Autoload)
 require __DIR__ . '/../vendor/autoload.php';
 
-// 3. Ambil instansiasi aplikasi Laravel sebelum dia berjalan
+// 2. Ambil instansiasi aplikasi Laravel
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// 4. Paksa Laravel menggunakan folder /tmp khusus serverless Vercel
+// 3. Paksa Laravel menggunakan folder /tmp khusus serverless Vercel
 $app->useStoragePath('/tmp/storage');
 
-// 5. Jalankan aplikasi seperti yang biasa dilakukan oleh public/index.php
+// 4. Jalankan aplikasi
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
