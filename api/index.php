de4009341310bf22f72e377a01873cca295bf613
akmal <akmaludin2333@gmail.com>
2026-06-29 16:05:44 +0700

    fix: adjust laravel storage path for serverless

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+
+// Mengarahkan ke file index asli Laravel
 require_once dirname(__DIR__) . '/public/index.php';
+
+// Memaksa Laravel menggunakan folder /tmp untuk menulis cache & session di Vercel
+$app->useStoragePath('/tmp/storage');
+$app->config->set('cache.stores.file.path', '/tmp/storage/framework/cache/data');
+$app->config->set('session.files', '/tmp/storage/framework/sessions');
+$app->config->set('view.compiled', '/tmp/storage/framework/views');
